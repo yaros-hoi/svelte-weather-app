c8a94c0b0d95a95b0873258268768a4888cd0ac8
Yaroslav Golovachev <golovachev.yaroslav@gmail.com>
2020-09-09 16:58:36 +0500

    Add files via upload

--- src/store/location.js
+++ src/store/location.js
@@ -1,25 +1,5 @@
 import { writable } from 'svelte/store';
 
-const initialValue = {
-  main: {
-    temp: '',
-  },
-};
-
-export const weather = writable(initialValue);
-export const loading = writable(false);
-
-export const getWeather = async (latitude, longitude) => {
-  loading.set(true);
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${OPEN_WEATHER_MAP_ID}&units=metric`, {
-    "method": "GET",
-  });
-  const json = await response.json();
-
-  weather.set(json);
-  loading.set(false);
-}
-
 export const location = writable({});
 
 export const getLocation = async (latitude, longitude) => {
